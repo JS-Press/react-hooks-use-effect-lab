0894bf5caec71ffe884b34b3e2005ec3080698f3
Jarad Solomon <jaradsolomon@gmail.com>
2021-08-02 01:06:31 -0500

    Completed Assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  
+  // useEffect(() => {
+  //   if (timeRemaining === 0)
+  //     {
+  //     setTimeRemaining(10)
+  //     }
+  //     else{
+  //     const newNum = timeRemaining - 1
+  //     setTimeout(setTimeRemaining(newNum),1000)
+  //     }    
+  // }, [])
 
   // add useEffect code
+
+  useEffect( () => {
+  const timer = setTimeout( () => {
+    if(timeRemaining > 0){
+const newNum = timeRemaining - 1
+setTimeRemaining(newNum)}else{const newNum = 10
+  setTimeRemaining(newNum)
+  onAnswered(false)}
+
+  }, 1000)
+  return () => clearTimeout(timer);
+  }, [timeRemaining]
+  )
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
